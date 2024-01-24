0f39f494e38c7f1e460099d68c54180bde4f94f6
Roch Deschaseaux <roch.deschaseaux@maf.fr>
2024-01-24 10:32:32 +0100

    afficher l'adresse de la web API GED avant de la démarrer

--- run_ged_web_api.ps1
+++ run_ged_web_api.ps1
@@ -1,1 +1,3 @@
-dotnet run --project "C:\TeamProjects\GED API\MAF.GED.API.Host\MAF.GED.API.Host.csproj" --urls="http://localhost:44363/"
+$ged_api_url = "http://localhost:44363/"
+"lauching GED API at:", $ged_api_url | %{ Write-Host $_ }
+dotnet run --project "C:\TeamProjects\GED API\MAF.GED.API.Host\MAF.GED.API.Host.csproj" --urls=$ged_api_url
